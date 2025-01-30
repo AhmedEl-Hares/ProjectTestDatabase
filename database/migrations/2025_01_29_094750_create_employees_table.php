eae36d9dcd2c311e92b170eb9b5be167aa11c85a
EL-HARES <ahmedelhares.work@gmail.com>
2025-01-30 16:25:16 +0200

    Added Some Comments in the employee Migration

--- database/migrations/2025_01_29_094750_create_employees_table.php
+++ database/migrations/2025_01_29_094750_create_employees_table.php
@@ -19,10 +19,12 @@ return new class extends Migration
             $table->string('Phone')->unique();
             $table->string('Address');
             $table->string('Nationality');
-            $table->string('Department');
-            $table->string('Position');
+            
+            // as a start we can use enums that has (HR, IT, Marketing, Sales, Finance, Operations,logistics) of we can do a table for departments and link it to the employees table
+            $table->string('Department'); 
+            $table->string('Position'); //as a start we can use enums that has (Manager,supervisor,employee)
             $table->decimal('Salary', 12, 3);
-            $table->string('Payment_Currency');
+            $table->string('Payment_Currency'); //as a start we can use enums that has (USD, EUR, EGP)
             $table->date('Hiring_Date');
             $table->date('Birth_Date');
             $table->integer('Age');
